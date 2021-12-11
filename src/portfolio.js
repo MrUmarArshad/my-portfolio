3adec3785c561503a247bdf6d9cb99225123bbb3
Muhammad Umar <muhammadumar@Muhammads-MacBook-Pro.local>
2021-12-11 08:17:22 +0500

    replace name

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,8 +11,8 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Muhammad Umar",
+  title: "Hey, I'm Umar",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
